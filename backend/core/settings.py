265b4b3b5aab332853a3ff1d0f3a651747d601a3
Suraj Kumar <surajkmr4218@gmail.com>
2026-02-12 10:35:14 -0500

    remove hardcoded database passwords

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -206,9 +206,9 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ.get('DB_NAME', 'hypercase'),
-            'USER': os.environ.get('DB_USER', 'postgres'),
-            'PASSWORD': os.environ.get('DB_PASSWORD', 'hypercase123'),
+            'NAME': os.environ['DB_NAME'],
+            'USER': os.environ['DB_USER'],
+            'PASSWORD': os.environ['DB_PASSWORD'],
             'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
             'PORT': os.environ.get('DB_PORT', '5432'),
         }
